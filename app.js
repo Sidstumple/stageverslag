1a4b0ee0ac7da2df573433e4d3a0e1235b1fc086
Sidstumple <cydstumpel@msn.com>
2017-12-22 16:14:48 +0100

    dignen

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ app.get('/', function(req, res) {
 	}); //renders index.ejs
 })
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
 	console.log('Starting server at port 3000');
 	if (process.send) {
 		process.send({
