f408e3ee9f04a843bde290cbbc1ea8cf7801c6ca
Sidstumple <cydstumpel@msn.com>
2018-01-11 10:20:50 +0100

    af?

--- app.js
+++ app.js
@@ -14,159 +14,157 @@ sass.render({
 
 var scope = {
 	chapters: [{
-		slug: 'inleiding',
-		title: 'Inleiding',
-		image: 'url("public/img/entrepotdok.png") no-repeat',
-		type: 'toc',
-		color: '#52d2c6',
+			slug: 'inleiding',
+			title: 'Inleiding',
+			image: 'url("public/img/entrepotdok.png") no-repeat',
+			type: 'toc',
+			color: '#52d2c6',
 
-		columnspan: 'span 1',
-		rowspan: 'span 2',
-		columnstart: '2',
-		rowstart: '2',
+			columnspan: 'span 1',
+			rowspan: 'span 2',
+			columnstart: '2',
+			rowstart: '2',
 
-		smallcolumnspan: 'span 1',
-		smallrowspan: 'span 2',
-		smallrow: '1',
-		smallcolumn: '2'
+			smallcolumnspan: 'span 1',
+			smallrowspan: 'span 2',
+			smallrow: '1',
+			smallcolumn: '2'
 
-	}, {
-		slug: 'over-matise',
-		title: 'Over Matise',
-		image: 'url("public/img/horse.jpg") no-repeat',
-		type: 'toc',
-		color: '#5ebfdd',
+		}, {
+			slug: 'over-matise',
+			title: 'Over Matise',
+			image: 'url("public/img/horse.jpg") no-repeat',
+			type: 'toc',
+			color: '#5ebfdd',
 
-		columnspan: 'span 2',
-		rowspan: 'span 1',
-		columnstart: '3',
-		rowstart: '1',
+			columnspan: 'span 2',
+			rowspan: 'span 1',
+			columnstart: '3',
+			rowstart: '1',
 
-		smallcolumnspan: 'span 2',
-		smallrowspan: 'span 1',
-		smallrow: '1',
-		smallcolumn: '3'
-	}, {
-		slug: 'over-mijn-begeleider',
-		title: 'Over mijn begeleider',
-		image: '#ff9189',
-		type: 'toc',
-		color: '#aedc78',
+			smallcolumnspan: 'span 2',
+			smallrowspan: 'span 1',
+			smallrow: '1',
+			smallcolumn: '3'
+		}, {
+			slug: 'over-mijn-begeleider',
+			title: 'Over mijn begeleider',
+			image: '#ff9189',
+			type: 'toc',
+			color: '#aedc78',
 
-		columnspan: 'span 1',
-		rowspan: 'span 1',
-		columnstart: '5',
-		rowstart: '1',
+			columnspan: 'span 1',
+			rowspan: 'span 1',
+			columnstart: '5',
+			rowstart: '1',
 
-		smallcolumnspan: 'span 1',
-		smallrowspan: 'span 2',
-		smallrow: '1',
-		smallcolumn: '5'
-	}, {
-		slug: 'werkzaamheden',
-		title: 'Werkzaamheden',
-		image: 'url("public/img/vimn.gif") no-repeat',
-		type: 'toc',
-		color: '#52d2c6',
+			smallcolumnspan: 'span 1',
+			smallrowspan: 'span 2',
+			smallrow: '1',
+			smallcolumn: '5'
+		}, {
+			slug: 'werkzaamheden',
+			title: 'Werkzaamheden',
+			image: 'url("public/img/vimn.gif") no-repeat',
+			type: 'toc',
+			color: '#52d2c6',
 
-		columnspan: 'span 2',
-		rowspan: 'span 2',
-		columnstart: '1',
-		rowstart: '4',
+			columnspan: 'span 2',
+			rowspan: 'span 2',
+			columnstart: '1',
+			rowstart: '4',
 
-		smallcolumnspan: 'span 2',
-		smallrowspan: 'span 2',
-		smallrow: '3',
-		smallcolumn: '1'
-	}, {
-		slug: 'leerdoelen',
-		title: 'Leerdoelen',
-		image: 'url("public/img/kurk.gif") no-repeat',
-		type: 'toc',
-		color: '#aedc78',
+			smallcolumnspan: 'span 2',
+			smallrowspan: 'span 2',
+			smallrow: '3',
+			smallcolumn: '1'
+		}, {
+			slug: 'leerdoelen',
+			title: 'Leerdoelen',
+			image: 'url("public/img/kurk.gif") no-repeat',
+			type: 'toc',
+			color: '#aedc78',
 
-		columnspan: 'span 2',
-		rowspan: 'span 3',
-		columnstart: '4',
-		rowstart: '3',
+			columnspan: 'span 2',
+			rowspan: 'span 3',
+			columnstart: '4',
+			rowstart: '3',
 
-		smallcolumnspan: 'span 2',
-		smallrowspan: 'span 2',
-		smallrow: '2',
-		smallcolumn: '3'
-	}, {
-		slug: 'observatie-opdrachten',
-		title: 'Observatie opdrachten',
-		image: '#52d2c6',
-		type: 'toc',
-		color: '#5ebfdd',
+			smallcolumnspan: 'span 2',
+			smallrowspan: 'span 2',
+			smallrow: '2',
+			smallcolumn: '3'
+		}, {
+			slug: 'observatie-opdrachten',
+			title: 'Observatie opdrachten',
+			image: '#52d2c6',
+			type: 'toc',
+			color: '#5ebfdd',
 
-		columnspan: 'span 1',
-		rowspan: 'span 2',
-		columnstart: '3',
-		rowstart: '2',
+			columnspan: 'span 1',
+			rowspan: 'span 2',
+			columnstart: '3',
+			rowstart: '2',
 
-		smallcolumnspan: 'span 1',
-		smallrowspan: 'span 1',
-		smallrow: '4',
-		smallcolumn: '3'
-	}, {
-		slug: 'analyse-feedbackformulieren',
-		title: 'Analyse feedbackformulieren',
-		image: 'url("public/img/nerf.jpg") no-repeat',
-		type: 'toc',
-		color: '#aedc78',
+			smallcolumnspan: 'span 1',
+			smallrowspan: 'span 1',
+			smallrow: '4',
+			smallcolumn: '3'
+		}, {
+			slug: 'analyse-feedbackformulieren',
+			title: 'Analyse feedbackformulieren',
+			image: 'url("public/img/nerf.jpg") no-repeat',
+			type: 'toc',
+			color: '#aedc78',
 
-		columnspan: 'span 2',
-		rowspan: 'span 1',
-		columnstart: '4',
-		rowstart: '2',
+			columnspan: 'span 2',
+			rowspan: 'span 1',
+			columnstart: '4',
+			rowstart: '2',
 
-		smallcolumnspan: 'span 2',
-		smallrowspan: 'span 1',
-		smallrow: '4',
-		smallcolumn: '4',
-	}, {
-		slug: 'conclusies',
-		title: 'Conclusies',
-		image: '#c68ad0',
-		type: 'toc',
-		color: '#52d2c6',
+			smallcolumnspan: 'span 2',
+			smallrowspan: 'span 1',
+			smallrow: '4',
+			smallcolumn: '4',
+		}, {
+			slug: 'conclusies',
+			title: 'Conclusies',
+			image: '#c68ad0',
+			type: 'toc',
+			color: '#52d2c6',
 
-		columnspan: 'span 1',
-		rowspan: 'span 2',
-		columnstart: '3',
-		rowstart: '4',
+			columnspan: 'span 1',
+			rowspan: 'span 2',
+			columnstart: '3',
+			rowstart: '4',
 
-		smallcolumnspan: 'span 1',
-		smallrowspan: 'span 1',
-		smallrow: '3',
-		smallcolumn: '5',
-	}, {
-		slug: 'stageplan',
-		title: 'Stageplan',
-		image: '#5ebfdd',
-		type: 'bijlage',
-		color: '#5ebfdd'
-	}, {
-		slug: 'verslag-nulmeting',
-		title: 'Verslag 0-meting',
-		image: '#ff8095',
-		type: 'bijlage',
-		color: '#52d2c6'
-	}, {
-		slug: 'feedbackformulieren',
-		title: 'Feedbackformulieren',
-		image: '#ff8095',
-		type: 'bijlage',
-		color: '#ffcd62'
-	}, {
-		slug: 'producten',
-		title: 'Producten',
-		image: '#ffcd62',
-		type: 'bijlage',
-		color: '#52d2c6'
-	}]
+			smallcolumnspan: 'span 1',
+			smallrowspan: 'span 1',
+			smallrow: '3',
+			smallcolumn: '5',
+		}, {
+			slug: 'verslag-nulmeting',
+			title: 'Verslag 0-meting',
+			image: '#ff8095',
+			type: 'bijlage',
+			color: '#52d2c6'
+		},
+		{
+			slug: 'stageplan',
+			title: 'Stageplan',
+			image: '#5ebfdd',
+			type: 'bijlage',
+			color: '#5ebfdd',
+			link: 'https://www.dropbox.com/s/vtlvh9q86ib8xa8/Stageplan_CydStumpel.pdf?dl=0'
+		}, {
+			slug: 'feedbackformulieren',
+			title: 'Feedbackformulieren',
+			image: '#ff8095',
+			type: 'bijlage',
+			color: '#ffcd62',
+			link: ''
+		}
+	]
 }
 
 app.use(express.static('./'))
